62ea27ee886e14d75e41758b848f250deaec1558
javie <javier_mera88@hotmail.com>
2017-02-19 16:18:55 -0600

    Added behavior to request location updates on device and store them in both the database, and in the activity as the last known location. Added new file to store the google map api key.

--- app/src/main/java/com/javier/positiontracker/MainActivity.java
+++ app/src/main/java/com/javier/positiontracker/MainActivity.java
@@ -1,25 +1,127 @@
 package com.javier.positiontracker;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.model.LatLng;
 import com.javier.positiontracker.databases.PositionTrackerDataSource;
+import com.javier.positiontracker.databases.PositionTrackerSQLiteHelper;
 import com.javier.positiontracker.model.UserLocation;
 
 import java.util.Date;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener{
+
+    public static final int FINE_LOCATION_CODE = 100;
+
+    private GoogleApiClient mClient;
+    private Location mLastLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LatLng latLng = new LatLng(20.00, -36.200);
-        long date = new Date().getTime();
-        UserLocation location = new UserLocation(latLng, date);
+
+        mClient = new GoogleApiClient.Builder(this)
+            .addConnectionCallbacks(this)
+            .addOnConnectionFailedListener(this)
+            .addApi(LocationServices.API)
+            .build();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, FINE_LOCATION_CODE);
+            return;
+        }
+
+        mClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if(mClient.isConnected()) {
+
+            LocationServices.FusedLocationApi.removeLocationUpdates(mClient, this);
+            mClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode) {
+
+            case FINE_LOCATION_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+
+                    mClient.connect();
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        LocationRequest request = LocationRequest.create()
+            .setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY)
+            .setInterval(2000L)
+            .setFastestInterval(1000L);
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(mClient, request, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
 
         PositionTrackerDataSource source = new PositionTrackerDataSource(this);
-        source.insertUserLocation(location);
+        UserLocation userLocation = new UserLocation(
+            new LatLng(location.getLatitude(), location.getLongitude()),
+            location.getTime()
+        );
+
+        source.insertUserLocation(userLocation);
+
+        mLastLocation = location;
     }
 }
