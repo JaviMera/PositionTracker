a2eafdae1b12099911541f9806ef375ef8bf7b52
javie <javier_mera88@hotmail.com>
2017-03-09 15:43:12 -0600

    Uncommented some important lines

--- app/src/main/java/com/javier/positiontracker/TrackerService.java
+++ app/src/main/java/com/javier/positiontracker/TrackerService.java
@@ -54,12 +54,12 @@ public class TrackerService extends Service
         mLocationCounter = new LocationCounter();
     }
 
-//    @Override
-//    public int onStartCommand(Intent intent, int flags, int startId) {
-//
-//        Toast.makeText(this, "on start command", Toast.LENGTH_SHORT).show();
-//        return START_STICKY;
-//    }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        Toast.makeText(this, "on start command", Toast.LENGTH_SHORT).show();
+        return START_STICKY;
+    }
 
     @Override
     public void onDestroy() {
